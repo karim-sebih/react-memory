366a49cd705015b1dbb4d9459bef04fd60758348
Jorge Santos <jorge.santos@laplateforme.io>
2025-05-22 10:26:07 +0200

    Memory card projet

--- react-memory/Memory/src/App.jsx
+++ react-memory/Memory/src/App.jsx
@@ -1,51 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import NewGameButton from './components/Button.jsx'
-
+import Cards from './components/Cards';
 function App() {
-  const [gameStarted, setGameStarted] = useState(false)
-  const [cards, setCards] = useState([])
-
-  // Dummy cards for demonstration
-  const initialCards = [
-    { id: 1, value: 'A', flipped: false, matched: false },
-    { id: 2, value: 'A', flipped: false, matched: false },
-    { id: 3, value: 'B', flipped: false, matched: false },
-    { id: 4, value: 'B', flipped: false, matched: false },
-  ]
-
-  const startNewGame = () => {
-    // Shuffle and reset cards
-    const shuffled = [...initialCards].sort(() => Math.random() - 0.5)
-    setCards(shuffled)
-    setGameStarted(true)
-  }
-
   return (
-    <>
-      <div>
+    <div className="App">
+        <h1>Memory Game</h1>
+        {/* <h3>Welcome to the Memory Game!</h3> */}
+        <h3>Try to match all the pairs of cards!</h3>
+        <Cards />
         
-        improve your memory and have some fun
-      
-      </div>
-      {!gameStarted ? (
-        <NewGameButton onNewGame={startNewGame} />
-      ) : (
-        <div>
-          
-          <h1>Memory Game</h1>
-          <div className="memory-board">
-            {cards.map(card => (
-              <div key={card.id} className="memory-card">
-                {card.flipped || card.matched ? card.value : "?"}
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-      
-    </>
-  )
+    </div>
+  );
 }
+  
 
-export default App
+export default App;
